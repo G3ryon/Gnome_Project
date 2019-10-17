bb72e7816bf3427a7f3678a7d30a7e804ae22227
Theo.ducoul <Dtheo98@gmail.com>
2019-10-17 11:50:37 +0200

    Update Gnome.java
    +Mapping for the price
    +Setting up the type of the variables
    +Setting up the Gnome constructor

--- Gnome/Model/Gnome.java
+++ Gnome/Model/Gnome.java
@@ -5,47 +5,58 @@ import java.util.*;
  * 
  */
 public class Gnome {
-
-    /**
+    PriceMap = {Colour={blue=69,red=666,white=1,black=1,yellow=0.5,metis=0.75},Sex={female=5,male=3,other=0.1},Size={extra_small=0.5,small=2,medium=1,large=0.5,extra_large=0.25},Build={thin=0.5,normal=1,thick=2,obese=0.25}}
+     /**
      * Default constructor
      */
-    public Gnome() {
+    public Gnome(int id,String name,float age,String skincolour,String size,String build,String sex) {
+        this.Id = id;
+        this.Name = name;
+        this.Age = age;
+        this.SkinColour = skincolour;
+        this.Size=size;
+        this.Build=build;
+        this.Sex=sex;
+        this.Price=this.GetPrice();
     }
+    /**
+     *
+     */
+    public int Id;
+    /**
+     * 
+     */
+    public float Age;
 
     /**
      * 
      */
-    public void Age;
+    public String SkinColour;
 
     /**
      * 
      */
-    public void SkinColour;
+    public float Size;
 
     /**
      * 
      */
-    public void Size;
+    public String Build;
 
     /**
      * 
      */
-    public void Build;
+    public String Sex;
 
     /**
      * 
      */
-    public void Sex;
+    public String Name;
 
     /**
      * 
      */
-    public void Name;
-
-    /**
-     * 
-     */
-    public void Price;
+    public float Price;
 
 
 
