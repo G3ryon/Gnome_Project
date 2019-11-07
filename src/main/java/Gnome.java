09790ce78f929db230f056aae656ef076f067f95
Theo.ducoul <Dtheo98@gmail.com>
2019-11-07 10:36:00 +0100

    Pricemap and getprice() in Gnome Class
    *Correction and right declaration of the pricemap and the getprice method

--- src/main/java/Gnome.java
+++ src/main/java/Gnome.java
@@ -1,57 +1,63 @@
 import java.util.*;
 
+
 /**
  * 
  */
 public class Gnome {
 
     private int Id;
-    private int Age;
+    private String Age;
     private String SkinColour;
-    private float Size;
+    private String Size;
     private String Build;
     private String Sex;
     private String Name;
-    private float Price;
+    private double Price;
 
-    PriceMap = {
-            Colour={
-                    blue=69,
-                    red=666,
-                    white=1,
-                    black=1,
-                    yellow=0.5,
-                    metis=0.75
-            },
-            Sex={
-                    female=5,
-                    male=3,
-                    other=0.1
-            },
-            Size={
-                    extra_small=0.5,
-                    small=2,
-                    medium=1,
-                    large=0.5,
-                    extra_large=0.25
-            },
-            Build={
-                    thin=0.5,
-                    normal=1,
-                    thick=2,
-                    obese=0.25
-            },
-            Age={
-                    less5=0,
-                    more5less8=0,
-                    more8less12=0,
-                    more12=0
-            }
+
+    private static Map<String,Map<String, Double>> PriceMap = new HashMap<String, Map<String, Double>>();
+    private static Map<String, Double> Caract = new HashMap<String, Double>();
+
+    static {
+        Caract.put("blue",69.0);
+        Caract.put("red",666.0);
+        Caract.put("white",1.0);
+        Caract.put("black",1.0);
+        Caract.put("yellow",1.0);
+        Caract.put("metis",1.0);
+        PriceMap.put("Colour",Caract);
+        Caract.clear();
+        Caract.put("female",69.0);
+        Caract.put("male",666.0);
+        Caract.put("other",1.0);
+        PriceMap.put("Sex",Caract);
+        Caract.clear();
+        Caract.put("extra_small",1.0);
+        Caract.put("small",3.0);
+        Caract.put("medium",2.0);
+        Caract.put("large",1.0);
+        Caract.put("extra_large",0.5);
+        PriceMap.put("Size",Caract);
+        Caract.clear();
+        Caract.put("thin",1.0);
+        Caract.put("normal",2.0);
+        Caract.put("thick",3.0);
+        Caract.put("obese",0.5);
+        PriceMap.put("Build",Caract);
+        Caract.clear();
+        Caract.put("less5",15.0);
+        Caract.put("more5less8",10.0);
+        Caract.put("more8less12",5.0);
+        Caract.put("more12",1.0);
+        PriceMap.put("Age",Caract);
+        Caract.clear();
     }
+
      /**
      * Default constructor
      */
-    public Gnome(int id,String name,int age,String skincolour,String size,String build,String sex) {
+    public Gnome(int id,String name,String age,String skincolour,String size,String build,String sex) {
         this.Id = id;
         this.Name = name;
 //        this.Age = age;
@@ -62,8 +68,8 @@ public class Gnome {
         this.Price=this.GetPrice();
     }
 
-    public void setId() {
-        this.Id
+    public void setId(Integer id) {
+        this.Id = id;
     }
 
     public void setName(String name)
@@ -136,14 +142,15 @@ public class Gnome {
         // TODO implement here
     }
 
-    public float GetPrice() {
-        int price = 0;
-        price += pricemap.get(colour.get(this.SkinColour));
-        price += pricemap.get(sex.get(this.Sex));
-        price += pricemap.get(Build.get(this.Build));
-        price += pricemap.get(Size.get(this.Size));
+    public double GetPrice() {
+       double Price = 0;
+        Price += PriceMap.get("Colour").get(this.SkinColour);
+        Price += PriceMap.get("Sex").get(this.Sex);
+        Price += PriceMap.get("Build").get(this.Build);
+        Price += PriceMap.get("Size").get(this.SkinColour);
+        Price += PriceMap.get("Age").get(this.Age);
 
-        return price ;
+        return Price ;
     }
 
 }
