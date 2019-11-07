c73ac48d889f7c671999e4d6544ce8b9f8095fc1
Theo.ducoul <Dtheo98@gmail.com>
2019-11-07 11:22:09 +0100

    testGnome
    +Gnome test interface

---
+++ src/test/java/GnomeTest.java
@@ -0,0 +1,71 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class GnomeTest extends Gnome {
+
+    @org.junit.jupiter.api.Test
+    void getPrice() {
+        
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetId() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetSkinColour() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetSize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetBuild() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetSex() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetAge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetSkinColour() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetSize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetBuild() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetSex() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void work() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void idle() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void handCrafting() {
+    }
+
+}
